2c64cb6a3e8a6ff630cc73ee3b7347ea35008342
jf-arce <josefranciscoarce21@gmail.com>
2025-04-14 16:57:04 -0300

    feat: add register view

--- app/src/main/java/com/appmoviles/tp1/MainActivity.kt
+++ app/src/main/java/com/appmoviles/tp1/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.appmoviles.tp1.models.User
 import com.appmoviles.tp1.ui.theme.Tp1Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +26,112 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Tp1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Register()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Tp1Theme {
-        Greeting("Android")
+fun Register() {
+    val name = remember { mutableStateOf("") }
+    val email = remember { mutableStateOf("") }
+    val password = remember { mutableStateOf("") }
+    val repeatPassword = remember { mutableStateOf("") }
+
+    val newUser = remember { mutableStateOf<User?>(null) }
+    val passwordVisible = remember { mutableStateOf(false) }
+    val repeatPasswordVisible = remember { mutableStateOf(false) }
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp)
+        .padding(top = 100.dp)) {
+
+        Text(
+            text = "Registrarse",
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier.padding(bottom = 24.dp)
+        )
+
+        TextField(
+            value = name.value,
+            onValueChange = { name.value = it },
+            label = { Text("Nombre") },
+            modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
+        )
+        TextField(
+            value = email.value,
+            onValueChange = { email.value = it },
+            label = { Text("Email") },
+            modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
+        )
+        TextField(
+            value = password.value,
+            onValueChange = { password.value = it },
+            label = { Text("Contraseña") },
+            visualTransformation = if (passwordVisible.value) VisualTransformation.None else PasswordVisualTransformation(),
+            trailingIcon = {
+                val image = if (passwordVisible.value)
+                    Icons.Filled.Visibility
+                else Icons.Filled.VisibilityOff
+
+                IconButton(onClick = {
+                    passwordVisible.value = !passwordVisible.value
+                }) {
+                    Icon(
+                        imageVector = image,
+                        contentDescription = if (passwordVisible.value) "Ocultar contraseña" else "Mostrar contraseña"
+                    )
+                }
+            },
+            modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
+        )
+        TextField(
+            value = repeatPassword.value,
+            onValueChange = { repeatPassword.value = it },
+            label = { Text("Repetir Contraseña") },
+            visualTransformation = if (repeatPasswordVisible.value) VisualTransformation.None else PasswordVisualTransformation(),
+            trailingIcon = {
+                val image = if (repeatPasswordVisible.value)
+                    Icons.Filled.Visibility
+                else Icons.Filled.VisibilityOff
+
+                IconButton(onClick = {
+                    repeatPasswordVisible.value = !repeatPasswordVisible.value
+                }) {
+                    Icon(
+                        imageVector = image,
+                        contentDescription = if (repeatPasswordVisible.value) "Ocultar contraseña" else "Mostrar contraseña"
+                    )
+                }
+            },
+            modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                if (password.value == repeatPassword.value) {
+                    newUser.value = User(name.value, email.value, password.value)
+                } else {
+                    newUser.value = null
+                }
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Registrar")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        newUser.value?.let {
+            Text("Usuario creado:")
+            Text("Nombre: ${it.name}")
+            Text("Email: ${it.email}")
+            Text("Contraseña: ${it.password}")
+        }
     }
 }
---
+++ app/src/main/java/com/appmoviles/tp1/models/User.kt
@@ -0,0 +1,7 @@
+package com.appmoviles.tp1.models
+
+class User(
+    var name: String,
+    var email: String,
+    var password: String
+){}
